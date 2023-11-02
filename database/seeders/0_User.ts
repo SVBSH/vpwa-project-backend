16eb1dcb2696a0c5e04fb35f5a349db5c0c424f0
SVBSH <getenv@protonmail.com>
2023-11-02 14:14:16 +0100

    add renamed User seeder

---
+++ database/seeders/0_User.ts
@@ -0,0 +1,12 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class extends BaseSeeder {
+  public async run() {
+    User.updateOrCreateMany('nickname', [
+      { nickname: 'foo', name: 'Jano', surname: 'Foo', password: '12345', state: 'offline', email: 'foo@example.com' },
+      { nickname: 'bar', name: 'Ondro', surname: 'Bar', password: '12345', state: 'online', email: 'bar@example.com' },
+      { nickname: 'baz', name: 'Jano', surname: 'Baz', password: '12345', state: 'dnd', email: 'baz@example.com' },
+    ])
+  }
+}
