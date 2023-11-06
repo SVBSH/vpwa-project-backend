01040c276ad6fa39af8c503a47a122d8856de29e
Branislav Trstenský <bt7s7k7@hotmail.sk>
2023-11-06 11:40:16 +0100

    Fixed User table seeder not awaiting

--- database/seeders/0_User.ts
+++ database/seeders/0_User.ts
@@ -3,7 +3,7 @@ import User from 'App/Models/User'
 
 export default class extends BaseSeeder {
   public async run() {
-    User.updateOrCreateMany('nickname', [
+    await User.updateOrCreateMany('nickname', [
       { nickname: 'foo', name: 'Jano', surname: 'Foo', password: '12345', state: 'offline', email: 'foo@example.com' },
       { nickname: 'bar', name: 'Ondro', surname: 'Bar', password: '12345', state: 'online', email: 'bar@example.com' },
       { nickname: 'baz', name: 'Jano', surname: 'Baz', password: '12345', state: 'dnd', email: 'baz@example.com' },
