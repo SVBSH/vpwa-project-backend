a9283257c101db5bbdf13202c6857364034e19c9
SVBSH <getenv@protonmail.com>
2023-11-11 17:41:23 +0100

    update

--- database/seeders/3_ChannelUser.ts
+++ database/seeders/3_ChannelUser.ts
@@ -5,10 +5,10 @@ import Channel from 'App/Models/Channel'
 export default class UserChannelSeeder extends BaseSeeder {
   public async run() {
     // Find users and channels to establish relationships
-    const user1 = await User.findBy('id', 1)
-    const user2 = await User.findBy('id', 2)
-    const channel1 = await Channel.findBy('id', 1)
-    const channel2 = await Channel.findBy('id', 2)
+    const user1 = await User.findBy('nickname', 'foo')
+    const user2 = await User.findBy('nickname', 'bar')
+    const channel1 = await Channel.findBy('name', 'general')
+    const channel2 = await Channel.findBy('name', 'Channel 1')
 
     if (user1 && channel1) {
       await user1.related('channels').attach([channel1.id])
