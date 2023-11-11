3cfc110da7af924f13e8a06f485086a3c5241a2e
SVBSH <getenv@protonmail.com>
2023-11-11 17:06:57 +0100

    Fetch users and use them in the seeder

--- database/seeders/2_Message.ts
+++ database/seeders/2_Message.ts
@@ -1,39 +1,53 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Channel from 'App/Models/Channel'
 import Message from 'App/Models/Message'
+import User from 'App/Models/User'
 
 export default class extends BaseSeeder {
   public async run() {
     const uniqueKey = 'content'
 
-    // TODO: fetch users
+    const userFoo = await User.findBy('nickname', 'foo')
+    const userBar = await User.findBy('nickname', 'bar')
+
+    if (userFoo === null || userBar === null) {
+      return
+    }
+
+    const channelGeneral = await Channel.findBy('name', 'general')
+    const channelChannel1 = await Channel.findBy('name', 'Channel 1')
+    if (channelGeneral === null || channelChannel1 === null) {
+      return
+    }
+
     await Message.updateOrCreateMany(uniqueKey,
       [
         {
-          channelId: 1,
-          createdBy: 2,
+          channelId: channelGeneral.id,
+          createdBy: userFoo.id,
           content: 'This is some message',
         },
         {
-          channelId: 1,
-          createdBy: 2,
+          channelId: channelGeneral.id,
+          createdBy: userFoo.id,
           content: 'More messages',
         },
         {
-          channelId: 1,
-          createdBy: 2,
+          channelId: channelGeneral.id,
+          createdBy: userFoo.id,
           content: 'loremOccaecat irure mollit magna ad consectetur.',
         },
         {
-          channelId: 1,
-          createdBy: 2,
+          channelId: channelGeneral.id,
+          createdBy: userBar.id,
           content: 'Voluptate nulla in dolore commodo eiusmod. Laboris\
            labore esse tempor fugiat velit nostrud. Sint ex ipsum do \
            esse officia cupidatat irure. Tempor sunt commodo nostrud \
            commodo esse. Sunt dolor ipsum excepteur irure eu do voluptate.',
         },
         {
-          channelId: 2,
-          createdBy: 1,
+          channelId: channelChannel1.id,
+          createdBy: userFoo.id,
           content: 'This is some message',
         },
       ])
