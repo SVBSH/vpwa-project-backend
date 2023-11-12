978ea9585e312f80c1d026e65d51414668ab407b
SVBSH <getenv@protonmail.com>
2023-11-12 18:12:10 +0100

    Add channel_admin reference to Channel model

--- database/seeders/1_Channel.ts
+++ database/seeders/1_Channel.ts
@@ -1,22 +1,32 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import Channel from 'App/Models/Channel'
+import User from 'App/Models/User'
+import Logger from '@ioc:Adonis/Core/Logger'
 
 export default class ChannelSeeder extends BaseSeeder {
   public async run() {
     const uniqueKey = 'name'
+    const channelAdmin = await User.findBy('nickname', 'foo')
+    if (!channelAdmin) {
+      Logger.warn('User <foo> not found. Seeder did not run.')
+      return
+    }
 
     await Channel.updateOrCreateMany(uniqueKey, [
       {
         name: 'general',
         isPublic: false,
+        channelAdmin: channelAdmin.id,
       },
       {
         name: 'Channel 1',
         isPublic: false,
+        channelAdmin: channelAdmin.id,
       },
       {
         name: 'Channel 2',
         isPublic: false,
+        channelAdmin: channelAdmin.id,
       },
     ])
   }
