5294e8bcc12cce13f1d6db1e7acd3c0fd9541d60
SVBSH <getenv@protonmail.com>
2023-11-19 15:46:03 +0100

    Create UserReository

--- config/repositories.ts
+++ config/repositories.ts
@@ -5,10 +5,28 @@ declare module '@ioc:Repositories/ChannelRepository' {
 
   export interface ChannelRepositoryContract {
     getChannelsForUser(user: User): Promise<Channel[]>
+    getBannedUsers(channel: Channel): Promise<User[]>
+    addUser(channel: Channel, userId: number): Promise<void>
+    getChannel(identifier: string | number): Promise<Channel | null>
+    getMessagesForChannel(channel: Channel)
+    createChannel(adminId: number, channelName: string, isPublic?: boolean): Promise<Channel>
+    hasMember(channel: Channel, userId: number): Promise<boolean>
+    isAdmin(channel: Channel, userId: number): boolean
   }
 
   const ChannelRepository: ChannelRepositoryContract
   export default ChannelRepository
+}
+
+declare module '@ioc:Repositories/UserRepository' {
+  import User from 'App/Models/User'
+
+  export interface UserRepositoryContract {
+    getUser(identifier: string | number): Promise<User | null>
+  }
+
+  const UserRepository: UserRepositoryContract
+  export default UserRepository
 }
 
 declare module '@ioc:Repositories/MessageRepository' {
