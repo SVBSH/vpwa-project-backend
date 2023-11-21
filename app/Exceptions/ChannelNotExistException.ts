6fcc60c8f6ff57b0b8adcbce3a21cd2567dd9ff4
SVBSH <getenv@protonmail.com>
2023-11-21 19:44:12 +0100

    Add custom exceptions

---
+++ app/Exceptions/ChannelNotExistException.ts
@@ -0,0 +1,8 @@
+import { Exception } from '@adonisjs/core/build/standalone'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ChannelNotExistException extends Exception {
+  public async handle(error: this, ctx: HttpContextContract) {
+    ctx.response.status(404).send(error.message)
+  }
+}
