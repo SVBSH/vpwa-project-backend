fdcaf19504b29d12514cce32ff75a8752a0f6886
SVBSH <getenv@protonmail.com>
2023-12-10 19:23:22 +0100

    add Model ChannelUserInvinations

---
+++ app/Models/ChannelUserInvintation.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import User from './User'
+import Channel from './Channel'
+
+export default class ChannelUserInvintation extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public hostId: number
+
+  @column()
+  public guestId: number
+
+  @column()
+  public channelId: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @belongsTo(() => User, {
+    foreignKey: 'hostId',
+  })
+  public host: BelongsTo<typeof User>
+
+  @belongsTo(() => User, {
+    foreignKey: 'guestId',
+  })
+  public guest: BelongsTo<typeof User>
+
+  @belongsTo(() => Channel, {
+    foreignKey: 'channelId',
+  })
+  public channel: BelongsTo<typeof Channel>
+}
